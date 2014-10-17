aad26789c9db0333b78eda9556fbd40c7fe810fc
Ayke van Laethem <aykevanlaethem@gmail.com>
2014-10-17 11:03:56 +0200

    Some documentation

--- apps/youtube/util.go
+++ apps/youtube/util.go
@@ -22,6 +22,9 @@ func zx() []byte {
 	return buf
 }
 
+// httpGetBody issues an HTTP request and returns the response body as a byte
+// array, or an error on HTTP protocol erroros or when the HTTP status code
+// isn't 200.
 func httpGetBody(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -32,6 +35,8 @@ func httpGetBody(url string) ([]byte, error) {
 	return processRequest(resp)
 }
 
+// httpPostFormBody is similar to httpGetBody, but does a POST request with
+// the supplied values.
 func httpPostFormBody(url string, values url.Values) ([]byte, error) {
 	resp, err := http.PostForm(url, values)
 	if err != nil {
@@ -42,6 +47,8 @@ func httpPostFormBody(url string, values url.Values) ([]byte, error) {
 	return processRequest(resp)
 }
 
+// processRequest downloads a HTTP response body. It will return an error when
+// the HTTP status code isn't 200.
 func processRequest(resp *http.Response) ([]byte, error) {
 
 	if resp.StatusCode != 200 {
@@ -60,6 +67,7 @@ func processRequest(resp *http.Response) ([]byte, error) {
 	}
 }
 
+// handle is a helper function for easier handling of fatal errors.
 func handle(err error, message string) {
 	if err != nil {
 		log.Fatalf("%s: %s\n", message, err)
